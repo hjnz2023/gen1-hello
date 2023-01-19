4ba5c7473043423195e3e821f93d54a490a14c81
Henry Jao <fivelinesofcode@gmail.com>
2023-01-19 18:28:18 +1300

    test: check existence of <router-outlet> element

--- apps/hello/src/app/app.component.spec.ts
+++ apps/hello/src/app/app.component.spec.ts
@@ -1,10 +1,12 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [RouterModule],
       declarations: [AppComponent, NxWelcomeComponent],
     }).compileComponents();
   });
@@ -21,12 +23,10 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('hello');
   });
 
-  it('should render title', () => {
+  it('should contain <router-outlet> element', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Welcome hello'
-    );
+    expect(compiled.querySelector('router-outlet')).not.toBeNull();
   });
 });
