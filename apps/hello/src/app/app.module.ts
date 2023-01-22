081a668ff1dc2644e48689fa7f1310493dfef1b6
Henry Jao <fivelinesofcode@gmail.com>
2023-01-22 15:39:46 +1300

    chore: setup root ngrx

--- apps/hello/src/app/app.module.ts
+++ apps/hello/src/app/app.module.ts
@@ -4,12 +4,28 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { extModules } from './build-specifics';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
     AppRoutingModule,
+    StoreModule.forRoot(
+      {},
+      {
+        metaReducers: [],
+        runtimeChecks: {
+          strictActionImmutability: true,
+          strictStateImmutability: true,
+        },
+      }
+    ),
+    // Instrumentation must be imported after importing StoreModule
+    extModules,
+    EffectsModule.forRoot([]),
   ],
   providers: [],
   bootstrap: [AppComponent],
