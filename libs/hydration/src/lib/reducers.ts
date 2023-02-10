4d89cd73bdfa89a2a016e2349bea7d77342705db
Henry Jao <fivelinesofcode@gmail.com>
2023-02-10 23:09:25 +1300

    refactor: better naming

--- libs/hydration/src/lib/reducers.ts
+++ libs/hydration/src/lib/reducers.ts
@@ -1,10 +1,11 @@
-import { isPlatformServer } from '@angular/common';
+import { isPlatformBrowser } from '@angular/common';
 import { Injector, PLATFORM_ID } from '@angular/core';
 import { RootState } from '@gen1-hello/shared';
 import { Action, ActionReducer, MetaReducer } from '@ngrx/store';
+
 import { rehydrate } from './actions';
 
-function actionIsRehydrate(
+function isRehydrateAction(
   action: Action | ReturnType<typeof rehydrate>
 ): action is ReturnType<typeof rehydrate> {
   return (
@@ -14,14 +15,13 @@ function actionIsRehydrate(
 }
 
 export function metaReducerFactory(injector: Injector): MetaReducer<RootState> {
-  const skipped = isPlatformServer(injector.get(PLATFORM_ID));
   let rehydrated = false;
+  const onBrowser = isPlatformBrowser(injector.get(PLATFORM_ID));
   return (reducer: ActionReducer<RootState>) => (state, action) => {
-    if (skipped || rehydrated || !actionIsRehydrate(action))
-      return reducer(state, action);
-
-    rehydrated = true;
-    console.log('Rehydrated successfully!');
-    return action.payload;
+    if (onBrowser && !rehydrated && isRehydrateAction(action)) {
+      rehydrated = true;
+      return action.payload;
+    }
+    return reducer(state, action);
   };
 }
